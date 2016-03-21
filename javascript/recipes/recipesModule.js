9479c37cef791ef44a89e6f5126044c19fc0c784
Waylon <wayloncn@gmail.com>
2016-03-21 18:42:33 -0400

    Implemented pagination for recipe list. Made changes so RecipeController can be access from other controllers

--- javascript/recipes/recipesModule.js
+++ javascript/recipes/recipesModule.js
@@ -25,23 +25,34 @@
 		return {
 			restrict: 'E',
 			templateUrl: 'html/recipes.html',
-			controller: ['$http', function($http) {
+			controller: ['$http', '$scope', function($http, $scope) {
 				
 				recipesController = this;
-
 				recipesController.recipes = [];
+
+				recipesController.totalItems = 0;
 
 				var fetchRecipesIndexUrl = 'http://localhost/MenuList/recipes.json';
 
 				$http.get(fetchRecipesIndexUrl).success(function(data){
-
 					recipesController.recipes = data.recipes;
-
+					recipesController.totalItems = recipesController.recipes.length;
+					console.log(recipesController.recipes.length);
 				});
 
 
 				this.imageRequestUrl = 'http://localhost/MenuList/recipes/image/';
 				this.addRecipeUrl = 'http://localhost/MenuList/recipes/add';
+
+				//pagination implementation
+				recipesController.itemsPerPage = 10;
+				recipesController.currentPage = 1;
+
+				$scope.setPage = function (pageNo) {
+					recipesController.currentPage = pageNo;
+				};
+
+
 
 			}],
 			controllerAs: 'recipesCtrl'
@@ -49,7 +60,7 @@
 	});	
 
 
-	recipesModule.controller('RecipesModalCreateCtrl', function($http, $scope, $uibModal, $log) {
+	recipesModule.controller('RecipesModalCreateCtrl', function($http, $scope, $uibModal) {
 		$scope.animationsEnabled = true;
 		var template;
 
@@ -75,16 +86,25 @@
 
 	recipesModule.controller('RecipesModalCreateInstanceCtrl', function($http, $scope, $uibModalInstance, uibModal) {
 		
-		// $scope.newRecipe = {
-		// 	user_id: 1,
-		// 	name: 'get new add page with success message',
-		// 	description: 'sss',
-		// 	instructions: 'sss',
-		// 	num_served: 4,
-		// 	image: 'no_image.jpg'
-		// }
+		$scope.newRecipe = {
+			user_id: 1,
+			name: 'get new add page with success message',
+			description: 'sss',
+			instructions: 'sss',
+			num_served: 4,
+			image: 'no_image.jpg',
+			private: true
+		}
 
-		$scope.newRecipe = {};
+		
+
+		//as the recipe is being created, the entered values will be kept in this variable. The 'private' property is controlled by a checkbox,
+		//so I must initialize its value to make sure it is sent in the request (otherwise, if the user does not interact with the checkbox,
+		//the 'private' property never gets initialized, which results in a failure to save the recipe in the back-end). Since 'true' is the default value in the back-end, I set the same default value here
+		// $scope.newRecipe = {
+		// 	user_id: 1, //this is for convenience until I implement users
+		// 	private: true
+		// };
 
 		$scope.cancel = function() {
 			$uibModalInstance.dismiss('cancel');
@@ -99,11 +119,20 @@
 
 			// $http.post('http://localhost/MenuList/recipes/add', $scope.newRecipe).then($scope.addSuccess(), $scope.addFail());
 
-
+			console.log($scope.newRecipe);
 			$http.post('http://localhost/MenuList/recipes/add', $scope.newRecipe).then( function addSuccess(response) {
 				console.log(response);
 				template = response.data;
+
+				//add the recipe add the beginning of the recipes array
 				recipesController.recipes.unshift($scope.newRecipe);
+
+				//update info for pagination and set back to first page
+				recipesController.currentPage = 1;
+				recipesController.totalItems += 1;
+				
+
+				console.log(recipesController.recipes.length);
 
 				open(uibModal, false, template);
 
