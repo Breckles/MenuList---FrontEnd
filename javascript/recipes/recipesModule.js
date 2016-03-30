d4e4cf27dc689e5a4a9c7d9baeed04ca10393c1f
Waylon <wayloncn@gmail.com>
2016-03-30 11:04:19 -0400

    Created forms for recipes and recipeIngredients so they are now on the front-end. Implemented creation of recipes along with their associated recipeIngredients

--- javascript/recipes/recipesModule.js
+++ javascript/recipes/recipesModule.js
@@ -5,6 +5,7 @@
 
 	var recipesController;
 	var recipeJustAdded;
+	var recipeHasNoIngredients;
 
 	//function to open the modal
 	//@uibModal : The uibModal service dependancy
@@ -42,6 +43,7 @@
 				var fetchRecipesIndexUrl = 'http://localhost/MenuList/recipes.json';
 
 				$http.get(fetchRecipesIndexUrl).success(function(data){
+					console.log(data.recipes[0]);
 					recipesController.recipes = data.recipes;
 					recipesController.totalItems = recipesController.recipes.length;
 					console.log(recipesController.recipes.length);
@@ -72,6 +74,7 @@
 		//calls the open function in the global scope, passes the $uibModal dependancy, as well as a true value for animation
 		$scope.open = function() {
 			recipeJustAdded = false;
+			recipeHasNoIngredients = false;
 			open($uibModal, true, 'html/recipes/recipesModalCreate.html');
 		}
 	});
@@ -79,17 +82,41 @@
 
 	recipesModule.controller('RecipesModalCreateInstanceCtrl', function($http, $scope, $uibModalInstance, uibModal) {
 		
+		var fetchUomsIndexUrl = 'http://localhost/MenuList/uoms.json';
+		var fetchIngredientsIndexUrl = "http://localhost/MenuList/ingredients.json";
+
+
+		$scope.uomsList = [];
+		$scope.ingredientsList = [];
+
 		$scope.newRecipe = {
 			user_id: 1,
-			name: 'get new add page with success message',
-			description: 'sss',
-			instructions: 'sss',
-			num_served: 4,
-			image: 'no_image.jpg',
-			private: true
-		}
+			description: '',
+			instructions: '',
+			image: '',
+			private: true,
+			recipe_ingredients: []
+		};
+
+		$scope.newRecipeIngredient = {
+			// recipe_id: -1,
+			// ingredient_id: 1,
+			// quantity: 1,
+			// uom_id: 1,
+			// instructions: ''
+		};
 
 		$scope.newRecipeIngredients = [];
+
+		//fetch all the available uoms
+		$http.get(fetchUomsIndexUrl).success(function(data) {
+			$scope.uomsList = data.uoms;
+		});
+
+		//fetch all available ingredients
+		$http.get(fetchIngredientsIndexUrl).success(function(data) {
+			$scope.ingredientsList = data.ingredients;
+		});
 		
 
 		//as the recipe is being created, the entered values will be kept in this variable. The 'private' property is controlled by a checkbox,
@@ -108,36 +135,67 @@
 			return recipeJustAdded;
 		};
 
+		$scope.recipeHasNoIngredients = function() {
+			return recipeHasNoIngredients;
+		};
+
+
+		$scope.addRecipeIngredient = function() {
+			$scope.newRecipeIngredients.push($scope.newRecipeIngredient);
+			$scope.newRecipeIngredient = {};
+		};
+
+		$scope.isNewRecipeIngredientsEmpty = function() {
+			return $scope.newRecipeIngredients.length === 0;
+		};
+
 
 		//this is messed up and I will have to find a different way to do things and implement best practice.
 		//essentially, the function makes the post request to create a new recipe. Upon success, it creates another 
 		//modal (without animation) with the response body, then closes the first one. This causes an unpleasant flicker on the screen.
 		//I did this because I could not find a way to simply swap the template for the existing modal.
-		$scope.submit = function() {			
+		$scope.submit = function() {	
+			recipeHasNoIngredients = false;
 
-			// $http.post('http://localhost/MenuList/recipes/add', $scope.newRecipe).then($scope.addSuccess(), $scope.addFail());
+			if($scope.newRecipeIngredients.length > 0) {
+				//The recipeIngredientsList elements must be formatted so that ingredient_id and uom_id have the proper id values, rather than the ingredient and uom objects
+				for(var i = 0; i < $scope.newRecipeIngredients.length; i++)
+				{
+					console.log($scope.newRecipeIngredients[i]);
+					$scope.newRecipeIngredients[i].uom_id = $scope.newRecipeIngredients[i].uom_id.id;
+					$scope.newRecipeIngredients[i].ingredient_id = $scope.newRecipeIngredients[i].ingredient_id.id;
+					console.log($scope.newRecipeIngredients[i]);
+				}
 
-			console.log($scope.newRecipe);
-			$http.post('http://localhost/MenuList/recipes/add', $scope.newRecipe).then( function addSuccess(response) {
-				// template = response.data;
+				//now assign the newRecipeIngredients array to newRecipe.ingredients
+				$scope.newRecipe.recipe_ingredients = $scope.newRecipeIngredients;
 
-				//add the recipe at the beginning of the recipes array
-				recipesController.recipes.unshift($scope.newRecipe);
+				console.log($scope.newRecipe);
+				$http.post('http://localhost/MenuList/recipes/add', $scope.newRecipe).then( function addSuccess(response) {
+					
 
-				//update info for pagination and set back to first page
-				recipesController.totalItems += 1;
-				recipesController.currentPage = 1;
+					//add the recipe at the beginning of the recipes array
+					recipesController.recipes.unshift($scope.newRecipe);
 
-				recipeJustAdded = true;
-				open(uibModal, false, 'html/recipes/recipesModalCreate.html');
+					//update info for pagination and set back to first page
+					recipesController.totalItems += 1;
+					recipesController.currentPage = 1;
 
-				$scope.newRecipe = {};
+					recipeJustAdded = true;
+					open(uibModal, false, 'html/recipes/recipesModalCreate.html');
 
-				$uibModalInstance.dismiss('cancel');
-			}, 
-			function addFail(response) {
-				console.log(response.status + "\n" + response.statusText);
-			});
+					$scope.newRecipe = {};
+
+					$uibModalInstance.dismiss('cancel');
+				}, 
+				function addFail(response) {
+					console.log(response.status + "\n" + response.statusText);
+				});
+			}
+			else {
+				recipeHasNoIngredients = true;
+			}
+
 		};
 	});
 })();
